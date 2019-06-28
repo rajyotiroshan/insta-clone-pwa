9ce35087d36c79edd6ee7529bf8bd42b65b144eb
rajan <rajangreedge@gmail.com>
2019-06-28 12:32:14 +0530

    prompt install banner

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -4,6 +4,23 @@ var closeCreatePostModalButton = document.querySelector('#close-create-post-moda
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
+  if(deferedPrompt) {//deferredPrompt is set.
+    //show the banner.
+    deferedPrompt.prompt();
+    //listen for user choice
+    //whether the user install it to the homescreen or not.
+    deferedPrompt.userChoice.then((choiceResult)=>{
+      console.log(choiceResult.outcome);
+
+      if(choiceResult.outcome === "dismissed") {
+        console.log("User canceled installation");
+      }else {
+        console.log("User installed to the home screen ");
+      }
+    });
+    deferedPrompt = nul;
+
+  }
 }
 
 function closeCreatePostModal() {
